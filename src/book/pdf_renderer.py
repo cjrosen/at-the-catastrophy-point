9fc930d73c783efcfd9f7952bcb67c96272d15a8
cj <cj.rosen@gmail.com>
2021-06-21 15:17:49 +0200

    render pdf page.

--- src/book/pdf_renderer.py
+++ src/book/pdf_renderer.py
@@ -16,118 +16,116 @@ class PdfPage(BookPage):
         return
 
 
-# class PdfRenderer(object):
+class PdfRenderer(object):
 
-    # def render_template(self, book: Book, id):
-    #     t = book.get_page_template(id)
-    #     if t is None:
-    #         print(f"ERROR: Page template with id '{id}' does not exist.")
-    #         return
-    #     s = SVG()
-    #     s.create(book.width_px * 2, book.height_px)
-    #     fill = '#E5D8C0'
-    #     stroke = '#000000'
-    #     stroke_width = 2
+    def render_template(self, book: Book, id):
+        t = book.get_page_template(id)
+        if t is None:
+            print(f"ERROR: Page template with id '{id}' does not exist.")
+            return
+            
+        filename = f"output/template_{id}.pdf"
+        c = canvas.Canvas(filename, (book.width_px * 2, book.height_px), 0)
+        stroke = Color(0,0,0)
+        stroke_width = 2
 
-    #     s.rectangle(0, 0, book.width_px * 2, book.height_px, fill, None, stroke_width, 0, 0)
-    #     s.line(book.width_px, 0, book.width_px, book.height_px, stroke, stroke_width)
+        c.setStrokeColor(stroke)
+        c.setLineWidth(stroke_width)
+        c.rect(0, 0, book.width_px * 2, book.height_px)
+        c.line(book.width_px, 0, book.width_px, book.height_px)
         
-    #     # left header
-    #     s.rectangle(
-    #         t['margins']['outer'],
-    #         t['margins']['top'] - t['header']['height'] - t['header']['offset'],
-    #         book.width_px - t['margins']['outer'] - t['margins']['inner'],
-    #         t['header']['height'],
-    #         fill, stroke, stroke_width, 0, 0)
-    #     # left footer
-    #     s.rectangle(
-    #         t['margins']['outer'],
-    #         book.height_px - t['margins']['bottom'] + t['footer']['offset'],
-    #         book.width_px - t['margins']['outer'] - t['margins']['inner'],
-    #         t['footer']['height'],
-    #         fill, stroke, stroke_width, 0, 0)
-    #     # left content
-    #     s.rectangle(
-    #         t['margins']['outer'],
-    #         t['margins']['top'],
-    #         book.width_px - t['margins']['outer'] - t['margins']['inner'],
-    #         book.height_px - t['margins']['top'] - t['margins']['bottom'],
-    #         fill, stroke, stroke_width, 0, 0)
+        # left header
+        c.rect(
+            t['margins']['outer'],
+            t['margins']['top'] - t['header']['height'] - t['header']['offset'],
+            book.width_px - t['margins']['outer'] - t['margins']['inner'],
+            t['header']['height'])
+        # left footer
+        c.rect(
+            t['margins']['outer'],
+            book.height_px - t['margins']['bottom'] + t['footer']['offset'],
+            book.width_px - t['margins']['outer'] - t['margins']['inner'],
+            t['footer']['height'])
+        # left content
+        c.rect(
+            t['margins']['outer'],
+            t['margins']['top'],
+            book.width_px - t['margins']['outer'] - t['margins']['inner'],
+            book.height_px - t['margins']['top'] - t['margins']['bottom'])
 
-    #     # right header
-    #     s.rectangle(
-    #         book.width_px + t['margins']['inner'],
-    #         t['margins']['top'] - t['header']['height'] - t['header']['offset'],
-    #         book.width_px - t['margins']['inner'] - t['margins']['outer'],
-    #         t['header']['height'],
-    #         fill, stroke, stroke_width, 0, 0)
-    #     # right footer
-    #     s.rectangle(
-    #         book.width_px + t['margins']['inner'],
-    #         book.height_px - t['margins']['bottom'] + t['footer']['offset'],
-    #         book.width_px - t['margins']['inner'] - t['margins']['outer'],
-    #         t['footer']['height'],
-    #         fill, stroke, stroke_width, 0, 0)
-    #     # right content
-    #     s.rectangle(
-    #         book.width_px + t['margins']['inner'],
-    #         t['margins']['top'],
-    #         book.width_px - t['margins']['inner'] - t['margins']['outer'],
-    #         book.height_px - t['margins']['top'] - t['margins']['bottom'],
-    #         fill, stroke, stroke_width, 0, 0)
-
-    #     s.finalize()
-
-    #     try:
-    #         filename = f"output/template_{id}.svg"
-    #         print(f"Save template rendering to {filename}")
-    #         s.save(filename)
-    #     except IOError as ioe:
-    #         print(ioe)
+        # right header
+        c.rect(
+            book.width_px + t['margins']['inner'],
+            t['margins']['top'] - t['header']['height'] - t['header']['offset'],
+            book.width_px - t['margins']['inner'] - t['margins']['outer'],
+            t['header']['height'])
+        # right footer
+        c.rect(
+            book.width_px + t['margins']['inner'],
+            book.height_px - t['margins']['bottom'] + t['footer']['offset'],
+            book.width_px - t['margins']['inner'] - t['margins']['outer'],
+            t['footer']['height'])
+        # right content
+        c.rect(
+            book.width_px + t['margins']['inner'],
+            t['margins']['top'],
+            book.width_px - t['margins']['inner'] - t['margins']['outer'],
+            book.height_px - t['margins']['top'] - t['margins']['bottom'])
 
 
-    # def render_page(self, page: SvgPage, output_path, render_meta = False):
-    #     s = SVG()
-    #     book = page.book
+        c.showPage()
+        try:
+            print(f"Save template rendering to {filename}")
+            c.save()
+        except IOError as ioe:
+            print(ioe)
 
-    #     # https://github.com/source-foundry/font-line
+
+    def render_page(self, page: PdfPage, output_path, render_meta = False):
         
-    #     font_size = book.px(12) * 0.5
-    #     line_height = font_size * 1.5
-    #     line = 0
-    #     lines = math.floor((page.content['h'] + (line_height - font_size)) / line_height)
-    #     s.add_font('default', 'Alte Haas Grotesk', font_size, line_height)
-    #     s.create(book.width_px, book.height_px)
-    #     fill = '#E5D8C0'
-    #     stroke = '#000000'
-    #     stroke_width = 2
+        book = page.book
+        filename = f"{output_path}/page_{page.number}.pdf"
+        c = canvas.Canvas(filename, (book.width_px * 2, book.height_px), 0)
 
-    #     s.rectangle(0, 0, book.width_px, book.height_px, fill, None, stroke_width, 0, 0)
-    #     if render_meta:
-    #         s.new_group()
-    #         s.rectangle(page.content['x'], page.content['y'], page.content['w'], page.content['h'], fill, '#DD0000', stroke_width, 0, 0)
-    #         for l in range(lines):
-    #             y = page.content['y'] - (line_height-font_size) * 0.8 + (l+1) * (line_height - 0.1)
-    #             #y = page.content['y'] + (l+1) * line_height
-    #             s.line(page.content['x'], y, page.content['x'] + page.content['w'], y, '#DD0000', stroke_width, dashes = book.px(1))
-    #         s.end_group()
-    #     # s.text_box(page.content['x'], page.content['y'], page.content['w'], page.content['h'],
-    #     #     ' '.join(["mkgÅjl"] * 100), 'default')
-    #     #s.text_box(page.content['x'], page.content['y'], page.content['w'], page.content['h'],
-    #     #    "En lång rackarns text, som strävar så ivrigt efter högerkanten att den förlorar fästet och, på grund av bredden, landar till vänster.", 'default')
-    #     #s.text(0, 0, "Hello World", 'default')
+        # https://github.com/source-foundry/font-line
+        
+        font_size = book.px(12) * 0.5
+        line_height = font_size * 1.5
+        line = 0
+        lines = math.floor((page.content['h'] + (line_height - font_size)) / line_height)
+        # s.add_font('default', 'Alte Haas Grotesk', font_size, line_height)
+        # s.create(book.width_px, book.height_px)
+        # fill = '#E5D8C0'
+        strokeColor = Color(0,0,0)
+        metaColor = Color(221,0,0)
+        stroke_width = 2
 
-    #     print(f"Try rende page of type: {type(page)}")
-    #     page.render(s)
+        c.setStrokeColor(strokeColor)
+        c.setLineWidth(stroke_width)
+        c.rect(0, 0, book.width_px, book.height_px)
+        if render_meta:
+            c.setLineWidth(1)
+            c.setStrokeColor(metaColor)
+            c.rect(page.content['x'], page.content['y'], page.content['w'], page.content['h'])
+            c.setDash(book.px(1), book.px(1))
+            for l in range(lines):
+                y = page.content['y'] - (line_height-font_size) * 0.8 + (l+1) * (line_height - 0.1)
+                #y = page.content['y'] + (l+1) * line_height
+                c.line(page.content['x'], y, page.content['x'] + page.content['w'], y)
+        # s.text_box(page.content['x'], page.content['y'], page.content['w'], page.content['h'],
+        #     ' '.join(["mkgÅjl"] * 100), 'default')
+        #s.text_box(page.content['x'], page.content['y'], page.content['w'], page.content['h'],
+        #    "En lång rackarns text, som strävar så ivrigt efter högerkanten att den förlorar fästet och, på grund av bredden, landar till vänster.", 'default')
+        #s.text(0, 0, "Hello World", 'default')
 
-    #     s.finalize()
+        #print(f"Try rende page of type: {type(page)}")
+        #page.render(s)
 
-    #     try:
-    #         filename = f"{output_path}/page_{page.number}.svg"
-    #         print(f"Save template rendering to {filename}")
-    #         s.save(filename)
-    #     except IOError as ioe:
-    #         print(ioe)
+        try:
+            print(f"Save template rendering to {filename}")
+            c.save()
+        except IOError as ioe:
+            print(ioe)
 
 
 if __name__ == '__main__':
@@ -135,6 +133,13 @@ if __name__ == '__main__':
     book.page_count_offset = 0
     book.add_page(BookPage('default'))
     book.add_page(BookPage('default'), number=-1) 
+
+    book.add_page(PdfPage('default', {
+            'text': "En liten text."
+        }, None), number=None)
+
+    book.update()
+    print(book)
 
     c = canvas.Canvas("output/hello.pdf", (book.width_px, book.height_px), 0)
 
@@ -164,3 +169,12 @@ if __name__ == '__main__':
 
     c.showPage()
     c.save()
+
+    # template
+    r = PdfRenderer()
+    r.render_template(book, 'default')
+
+    # page
+    page = book.get_page(3)
+    print(type(page))
+    r.render_page(page, 'output', True)
